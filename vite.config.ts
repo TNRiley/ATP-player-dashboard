8aab9729ae658769bbae02dd4ec1a6a23109369d
Trevor Riley <tnriley@gmail.com>
2025-11-11 13:13:36 -0500

    feat: Add build artifacts for /docs deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/atp-player-dashboard/",
+  base: "./", 
   plugins: [react()],
+  
+  build: {
+    outDir: "docs"
+  },
+  
   server: {
     port: 3000,
     open: true
-  },
+  }
 });
-
